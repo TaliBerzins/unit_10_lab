ac3dc4b8c8acf81b5d853b617acabd4cf0d0ebf1
TaliBerzins <tali.berzins@outlook.com>
2026-03-29 17:04:57 -0400

    Checked if loops for reading file and adding to dictionary worked

--- Lab10_tberzins-1.py
+++ Lab10_tberzins-1.py
@@ -22,10 +22,23 @@ class WordAnalyzer:
 
     def process_file(self):
         with open(self.__filepath, 'r') as file:
+            #For loop to create each line to a list without punctuation and all lower case of words
             for line in file:
                 translator = str.maketrans('', '', string.punctuation)
                 clean_s = line.translate(translator)
+                clean_s = clean_s.lower()
                 print(clean_s)
+                word_list = clean_s.split()
+                #Updating the count of each word in the dictionary
+                for i in range(len(word_list)):
+                    if word_list[i] in self.__frequencies:
+                      self.__frequencies[word_list[i]]+=1
+                    else:
+                      self.__frequencies[word_list[i]]=1
+                    
+                    
+                    
+                
 
 
 
